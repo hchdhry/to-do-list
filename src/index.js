5f4a1ae47124d10a5bfab6ec6e73b48da5dcc0f3
hassan <hchoudhury20@hotmail.com>
2023-06-13 15:21:44 +0100

    adds functionality to dynamically generate divs for project list

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import { task } from "./yee"
 import make from "./array"
 
 const button=document.querySelector("button")
+const project= document.getElementById("project-list")
 
+const array=[];
 button.addEventListener("click",function(event){
     event.preventDefault();
     const title = document.getElementById('title').value;
@@ -11,22 +13,41 @@ button.addEventListener("click",function(event){
     const priority = document.getElementById('priority').value;
     const yee =new task(title,description,dueDate,priority)
     array.push(yee)
+    console.log(array[2])
+    createtask()
    
 })
 
-const array=[];
-
-for(i=0;i>array.length;i++){
-    row=document.createElement('tr')
-    tcell=document.createElement('td')
-    dcell=document.createElement('td')
-    ucell=document.createElement('td')
-    pcell=document.createElement('td')
+function createtask(){
+    for(let i=0;i<=array.length;i++){
+        var activity=array[i];
+        console.log(activity);
+       
+        const row = document.createElement('tr');
+        const tcell = document.createElement('td');
+        const dcell = document.createElement('td');
+        const ucell = document.createElement('td');
+        const pcell = document.createElement('td');
+    
+         
+        project.appendChild(row);
+        row.appendChild(tcell);
+        row.appendChild(dcell);
+        row.appendChild(ucell);
+        row.appendChild(pcell);
+    
+        tcell.textContent=activity.title;
+        dcell.textContent=activity.description;
+        ucell.textContent=activity.dueDate;
+        pcell.textContent=activity.priority;
+        
     
     
-   row.classlist.add("row")
+    
+    
+    }}
 
 
 
-}
+
 
